3f1d09c82484f16a2a604a1a555ba846b18fc559
Ramy Hakam <ramyhakam2@gmail.com>
2017-04-26 22:14:01 +0200

    fix bugs adding Delete Object  method

--- src/ParseCurl.php
+++ src/ParseCurl.php
@@ -1,5 +1,8 @@
 <?php 
 final class ParseCurl{
+
+
+//create a new object 
 public function createObject($app_id, $masterkey, $url, $data){
 	$cpt = curl_init($url);
 	curl_setopt($cpt, CURLOPT_HTTPHEADER, array(
@@ -15,11 +18,11 @@ public function createObject($app_id, $masterkey, $url, $data){
 
 	]);
 
-	$result = curl_exec($cpt);
+return	$result = curl_exec($cpt);
 
 
 }
-
+//get an object from a parse server  
 public function getObject($app_id, $masterkey, $url, $query=""){
 	$cpt = curl_init($url.$query);
 
@@ -31,10 +34,10 @@ public function getObject($app_id, $masterkey, $url, $query=""){
 	curl_setopt($cpt, CURLOPT_RETURNTRANSFER, 1);
 
 	$result = curl_exec($cpt);
-	//return json_decode($result);
 		return $result;
 }
 
+//update an object 
 
 public function updateObject($app_id, $masterkey, $url, $data){
 	$cpt = curl_init($url);
@@ -60,6 +63,30 @@ public function updateObject($app_id, $masterkey, $url, $data){
 
 
 
+//delete an object 
+
+
+public function deleteObject($app_id, $masterkey, $url, $data=" "){
+	$cpt = curl_init($url);
+	curl_setopt($cpt, CURLOPT_HTTPHEADER, array(
+		'X-Parse-Application-Id: '.$app_id,
+		'X-Parse-Master-Key: '.$masterkey,
+		'Content-Type: application/json'
+	));
+
+	curl_setopt($cpt, CURLOPT_RETURNTRANSFER, 1);
+
+	curl_setopt($cpt, CURLOPT_CUSTOMREQUEST, "DELETE");
+
+	$result = curl_exec($cpt);
+
+	return $result;
+}
+
+
+
+//Query  on a parse server
+
 public function queryParse($app_id, $masterkey, $url, $query=""){
 
 	$cpt = curl_init($url.$query);
@@ -74,6 +101,8 @@ public function queryParse($app_id, $masterkey, $url, $query=""){
 	$result = curl_exec($cpt);
 	return json_decode($result);
 }
+
+//get number of objects
 
 public function countParse($app_id, $masterkey, $url, $query=""){
 	$cpt = curl_init($url.$query);
